00e1e6fb6c30e617c47fca66e4932c95fe9d911a
mukwayaedreen <edmukwaya@gmail.com>
2022-07-02 14:49:54 -0700

    size of datatype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf(" Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
-	printf("Size of a float: %f byte(s)\n", sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", sizeof(d));
 	return (0);
 }
