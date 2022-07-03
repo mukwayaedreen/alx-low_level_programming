797a63f5bebea2c9c6169f516c43c1307ddee6ed
mukwayaedreen <edmukwaya@gmail.com>
2022-07-03 06:49:02 -0700

    alphatabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	/* this is an ascii code for new line */
+	return (0);
+}
+ 
