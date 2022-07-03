116586b37ffe1737d08359d5d970f0bc00612e80
mukwayaedreen <edmukwaya@gmail.com>
2022-07-03 04:55:55 -0700

    checking for a random number to see

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,37 +3,25 @@
 
 #include <time.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (success)
+ */
 
 int main(void)
-
 {
+ int n;
 
-		int n;
+ srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n>0)
+printf("%d is positive\n", n);
+if(n==0)
+printf("%d is zero", n);
+if (n<0)
+printf("%d is negative", n);
 
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					/* your code goes there */
-				if(n>0)
-				{
-					printf("is positive");
-				}
-				else if(n==0)
-				{
-					printf("is zero");
-				}
-				else
-				{
-					printf("is negative");
-
-					return (0);
+return (0);
 
 }
