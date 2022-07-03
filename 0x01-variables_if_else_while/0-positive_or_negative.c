b894e086b884bde8956238e6d10813915b412022
mukwayaedreen <edmukwaya@gmail.com>
2022-07-03 05:10:16 -0700

    checking for a random number to see

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@
 
 int main(void)
 {
- int n;
+int n;
 
- srand(time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if(n > 0)
 printf("%d is positive\n", n);
-if(n==0)
+if(n == 0)
 printf("%d is zero", n);
-if (n<0)
+if (n < 0)
 printf("%d is negative", n);
 
 return (0);
