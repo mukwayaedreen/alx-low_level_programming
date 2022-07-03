b27714cb99e803071d00fe0590c9e6eae7781e2b
mukwayaedreen <edmukwaya@gmail.com>
2022-07-03 06:16:11 -0700

    print last digit of random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,19 +22,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 y = n % 10;
-printf("Last digit of %d is %d\n", n, y);
 
-if (y>5)
+if (y > 5)
 {
-printf("and is greater than 5");
+printf("Last digit of %d is %d and is greater than 5\n", n, y);
 }
 if (y == 0)
 {
-printf("and is 0");
+printf("Last digit of %d is %d and is zero\n", n, y);
 }
 if (y < 6 && y != 0)
 {
-printf("and is less than 6 and not 0");
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);  
+	
 }
 
 printf("\n");
