2a2a28362d70a8976798689d0053f4284215f5fe
mukwayaedreen <edmukwaya@gmail.com>
2022-07-06 06:29:46 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs -> computes the absolute value of a number
+ *@c: parameter to hold the absolute value
+ *Return:Absolute value
+ */
+
+int _abs(int c)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
