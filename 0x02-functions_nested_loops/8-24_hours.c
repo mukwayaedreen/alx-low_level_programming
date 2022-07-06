20ca1aad10bab370244daf84c5b50b70480d818b
mukwayaedreen <edmukwaya@gmail.com>
2022-07-06 07:41:59 -0700

    prints 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints 24 hours
+*Return: Always 0 (success)
+*/
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			if (a < 10)
+			{
+				_putchar('0');
+				_putchar(a + '0');
+			}
+			else if (a >= 10)
+			{
+				_putchar((a / 10) + '0');
+				-putchar((a % 10) + '0');
+			}
+			if (b < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(b + '0');
+			}
+			else if (b > 10)
+			{
+				_putchar(':');
+				_putchar((b / 10) + '0');
+				_putchar((b % 10) + '0');
+			}
+		}
+}
