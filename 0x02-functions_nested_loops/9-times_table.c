f4afe1b4a61fe524e792a82f67f6596e9d5ca17b
mukwayaedreen <edmukwaya@gmail.com>
2022-07-06 09:01:55 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,32 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, k, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(j);
+			k = j * i;
+
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
