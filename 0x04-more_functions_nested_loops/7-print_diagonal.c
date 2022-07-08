e49772c7083d11514440d2203739c7adddecbdcf
mukwayaedreen <edmukwaya@gmail.com>
2022-07-08 06:50:22 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints diagonal
+* @n: numbers of lines
+* Return: Always 0
+*/
+
+void print_diagonal(int n)
+{
+	int x, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+			{
+				_putchar(' ');
+				_putchar(92);
+				_putchar('\n');
+			}
+		}
+	}
+}
