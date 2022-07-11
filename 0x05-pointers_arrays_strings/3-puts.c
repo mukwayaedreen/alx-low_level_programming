36b0a3d5057952afa87c0f02f7a3562f7cd1d36c
mukwayaedreen <edmukwaya@gmail.com>
2022-07-11 02:52:13 -0700

    printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - function prints a string
+* @str: variable consisting of string
+* Return: always 0
+*/
+
+void _puts(char *str)
+{
+	return (puts(str));
+}
