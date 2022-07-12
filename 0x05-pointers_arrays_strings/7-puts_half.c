bedc22b06fffb6c510cc782935a05ea19253334f
mukwayaedreen <edmukwaya@gmail.com>
2022-07-12 03:06:59 -0700

    last half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
 * puts_half - prints the last half of the string
@@ -12,11 +13,10 @@ void puts_half(char *str)
 	int a, n;
 
 	n = strlen(str);
-	a = n / 2;
 
 	if (n % 2 == 0)
 	{
-		for (a; a < n; a++)
+		for (a = n / 2; a < n; a++)
 		{
 			putchar(str[a]);
 		}
@@ -24,8 +24,8 @@ void puts_half(char *str)
 	else
 	{
 		n = (strlen(str) - 1) / 2;
-		a = n / 2;
-		for (a; a < n; a++)
+
+		for (a = n / 2; a < n; a++)
 		{
 			putchar(str[a]);
 		}
