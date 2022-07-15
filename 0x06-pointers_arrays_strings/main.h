a45e4f0a7cd5bc6b0de6ed54e78dbb25af02e0af
mukwayaedreen <edmukwaya@gmail.com>
2022-07-15 02:14:26 -0700

    copying a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
-
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
