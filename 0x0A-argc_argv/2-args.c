4e7810785ca8bbc5670fa2684267dc11aa2e2d93
mukwayaedreen <edmukwaya@gmail.com>
2022-07-23 02:04:57 -0700

    printing all arguments passed to the function

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,14 +7,9 @@
 * Return: 0
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int a;
-
-	for (a = 0; a < argc; a++)
-	{
-		printf("%s\n ", argv[a]);
-	}
-
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
